e7919bd317f8c3f205f42462f9c78e4169e3a198
Ericg411 <ericg411@gmail.com>
2025-06-06 17:29:36 -0500

    made algorithm faster

--- solution.py
+++ solution.py
@@ -2,32 +2,25 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        word_exists = False
-        def exists(r, c, word_index, used):
-            nonlocal word_exists
-            if word_index == len(word):
-                word_exists = True
-                return
-            else:
-                directions = [(1,0),(-1,0),(0,1),(0,-1)]
-                for (dr, dc) in directions:
-                    nr = r + dr
-                    nc = c + dc
-                    if 0 <= nr < len(board) and 0 <= nc < len(board[0]) and (nr, nc) not in used and board[nr][nc] == word[word_index]:
-                        used.add((nr, nc))
-                        exists(nr, nc, word_index + 1, used)
-                        used.remove((nr, nc))
+        rows, cols = len(board), len(board[0])
 
+        def dfs(r, c, i):
+            if i == len(word):
+                return True
+            if r < 0 or c < 0 or r >= rows or c >= cols or board[r][c] != word[i]:
+                return False
 
+            temp = board[r][c]
+            board[r][c] = '#'  # Mark as visited
+            found = (dfs(r + 1, c, i + 1) or dfs(r - 1, c, i + 1) or
+                     dfs(r, c + 1, i + 1) or dfs(r, c - 1, i + 1))
+            board[r][c] = temp  # Restore original value
 
+            return found
 
-        for row in range(0, len(board)):
-            for col in range(0, len(board[0])):
-                if board[row][col] == word[0]:
-                    used = set()
-                    used.add((row, col))
-                    exists(row, col, 1, used)
-                    if word_exists:
-                        return True
-            
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == word[0] and dfs(r, c, 0):
+                    return True
+
         return False
